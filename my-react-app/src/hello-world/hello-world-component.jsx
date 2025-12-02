9ff3d15e6a4ea8ba39bf3729c8bc00683d2bdea2
NISANTHU-P <nisanthupalanisamy2006@gmail.com>
2025-12-02 09:27:07 +0530

    Created new react app

---
+++ my-react-app/src/hello-world/hello-world-component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloWorldComponent = () => {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      <p>Welcome to my React application.</p>
+    </div>
+  );
+};
+
+export default HelloWorldComponent;
